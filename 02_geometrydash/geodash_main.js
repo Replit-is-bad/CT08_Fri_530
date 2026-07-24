afc8b39e6666c7e67afdd7336a0c621488036858
Replit-is-bad <ylchia_hill@hotmail.com>
2026-07-24 10:40:48 +0000

    Jul 24, 2026, 10:40 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -1,21 +1,20 @@
 //player box
-let player;
-let box;
-let bg
+let player; // Player sprite
+let box;    // Player sprite image
+let bg;     // Background image
 
 // game variables
 const TILE_SIZE = 50;
-let tileMap1;
-let tileMap2;
-let ground;
-let spikes;
-let orb;
-let finishLine;
-// world building groups
 
+// world building groups
+let tileMap1;
+let ground;     // Ground sprite group
+let spikes;     // Spike sprite group
+let orbs;        // Orb sprite group
+let finishLine; // Finish line sprite group
 
 // image sprites
-
+let spike;
 
 // menu
 
@@ -24,65 +23,67 @@ let finishLine;
 
 
 function preload() {
-    box = loadImage("assets/cube2.png");
+    box = loadImage("assets/cube.png");
     bg = loadImage("assets/geobg.png");
     spike = loadImage("assets/spike.png");
 
     tileMap1 = loadStrings("stages/tiles1.txt");
-    tileMap2 = loadStrings("stages/tiles2.txt");
 }
 
 function setup() {
-    new Canvas(700,600); //Width,height
+    new Canvas(700, 600);   // (width, height)
     world.gravity.y = 32;
 
-    player = new Sprite(50,50,TILE_SIZE,TILE_SIZE); // x,y,width,height
+    // Player sprite
+    player = new Sprite(TILE_SIZE, TILE_SIZE, TILE_SIZE, TILE_SIZE);  // (x, y, width, height)
     player.img = box;
     player.friction = 0;
-    player.bounciness = 10;
-    player.collider = "static";
+    player.bounciness = 0;
+    player.collider = "none";
 
+    // Spawn point [x, y]
     startCoordinate = [TILE_SIZE, height - TILE_SIZE];
     player.x = startCoordinate[0];
     player.y = startCoordinate[1];
 
     // Ground sprite group
     ground = new Group();
-    ground.tile = "g";
-    ground.w = TILE_SIZE;
-    ground.h = TILE_SIZE;
-    ground.color = 'black';
-    ground.stroke = 'white';
-    ground.collider ='static';
+    ground.tile = "g";  // "g" represents a ground tile
+    ground.w = TILE_SIZE;   // Width
+    ground.h = TILE_SIZE;   // Height
+    ground.color = "black"; // Tile colour
+    ground.stroke = "white";    // Outline colour
+    ground.collider = "static"; // Cannot move
 
-    // spike
-    spike = new Group();
-    spike.tile = "s";
-    spike.image = spike;
-    spike.collider = 'static';
+    // Spikes sprite group
+    spikes = new Group();
+    spikes.tile = "s";
+    spikes.img = spike;
+    spikes.collider = "static";
 
-    // Orb
-    orb = new Group();
-    orb.tile = 'o';
-    orb.d =24;
-    orb.color ="#fffb00";
-    orb.stroke = 'white';
-    orb.collider ='static';
-    // finish
-    finish = new Group();
-    finish.tile = 'f';
-    finish.h =height*2;
-    finish.w = TILE_SIZE;
-    finish.color ="#005eff";
-    finish.stroke = 'white';
-    finish.collider ='static';
-    finish.visible = true;
+    // Orbs sprite group
+    orbs = new Group();
+    orbs.tile = "o";
+    orbs.d = 24;    // Diameter
+    orbs.color = "#f5d402";
+    orbs.stroke = "white";
+    orbs.collider = "static"; // Cannot move
 
-    // map
-    new Tiles(tileMap2, 0 ,0,TILE_SIZE,TILE_SIZE);
-}   
+    // Finish line sprite group
+    finishLine = new Group();
+    finishLine.tile = "f";
+    finishLine.w = TILE_SIZE;
+    finishLine.h = height * 2;
+    finishLine.color = "#f59402";
+    finishLine.stroke = "black";
+    finishLine.collider = "static";
+    finishLine.visible = true; // Show or hide
+
+    // Create map using tile map and sprite groups
+    new Tiles(tileMap1, 0, 0, TILE_SIZE, TILE_SIZE);    // (map, x, y, width, height)
+}
 
 function draw() {
-    clear();
-    image(bg,0 ,0, 800, 600); // IMAGE,X,Y,width,height
+    clear();    // Clear the previous frame before drawing
+    image(bg, 0, 0, 800, 600);  // (image, x, y, width, height)
 }
