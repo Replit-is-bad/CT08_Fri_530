34ad6a319ac64cdf230fb835f93625b355800b07
Replit-is-bad <ylchia_hill@hotmail.com>
2026-07-24 10:32:17 +0000

    Jul 24, 2026, 10:32 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -71,7 +71,7 @@ function setup() {
     // finish
     finish = new Group();
     finish.tile = 'f';
-    finish.h =height;
+    finish.h =height*2;
     finish.w = TILE_SIZE;
     finish.color ="#005eff";
     finish.stroke = 'white';
