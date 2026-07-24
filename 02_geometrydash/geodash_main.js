65ece5d821c7cae70c85fa4e677e10c889310e04
Replit-is-bad <ylchia_hill@hotmail.com>
2026-07-24 10:54:11 +0000

    Jul 24, 2026, 10:54 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -34,6 +34,7 @@ function setup() {
     new Canvas(700, 600);   // (width, height)
     world.gravity.y = 32;
 
+    player
     // Player sprite
     player = new Sprite(TILE_SIZE, TILE_SIZE, TILE_SIZE, TILE_SIZE);  // (x, y, width, height)
     player.img = box;
