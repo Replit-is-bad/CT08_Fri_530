981b5e499a8144f60e7051e242f935ca3d6846b4
Replit-is-bad <ylchia_hill@hotmail.com>
2026-07-31 09:55:59 +0000

    Jul 31, 2026, 9:55 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -6,7 +6,7 @@ let bg;     // Background image
 // game variables
 const TILE_SIZE = 50;
 const MAX_JUMP = 1;
-let jumpChance;
+let jumpChance = MAX_JUMP;
 // world building groups
 let tileMap1;
 let ground;     // Ground sprite group
@@ -35,7 +35,7 @@ function setup() {
     new Canvas(700, 600);   // (width, height)
     world.gravity.y = 32;
 
-    playerJump = 0;
+    jumpChance = MAX_JUMP;
     // Player sprite
     player = new Sprite(TILE_SIZE, TILE_SIZE, TILE_SIZE, TILE_SIZE);  // (x, y, width, height)
     player.img = box;
@@ -100,14 +100,14 @@ function draw() {
 
     if (kb.presses("space") || mouse.presses("left")) {
         player.vel.y = -11;
-        box.rotateTo(box.rotation + 359, 15);
+        player.rotateTo(player.rotation + 359, 15);
         jumpChance -= 1;
-
     }
 
-    if (box.collides(ground) && jumpChance <MAX_JUMP) {
-        jumpChance = MAX_JUMP
+    if (player.collides(ground) && jumpChance < MAX_JUMP) {
+        jumpChance = MAX_JUMP;
     }
+
     // if (box.collides(sharp)) {
     //     resetGame();
     // }
