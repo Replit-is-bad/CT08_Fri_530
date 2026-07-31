a82650b2c85c465c6aa5af61f5e5ed67248e28f8
Replit-is-bad <ylchia_hill@hotmail.com>
2026-07-31 10:29:13 +0000

    Jul 31, 2026, 10:29 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -117,7 +117,7 @@ function draw() {
         jumpChance = MAX_JUMP;
     }
     
-    if (player.collides(ground) && player.y >= ground.y - TILE_SIZE/2) {
+    if (player.collides(ground) && player.y > ground.y - TILE_SIZE/2) {
         resetGame();
     }
 
