7c613f0df68b17363b0b26d8bcc428f5f9e96656
Replit-is-bad <ylchia_hill@hotmail.com>
2026-08-07 10:26:54 +0000

    Aug 7, 2026, 10:26 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -140,7 +140,7 @@ function loadLevel() {
     if (level === 1) {
         new Tiles(tileMap1, 0, 0, 50, 50);
     }else if (level === 2){
-        new Tiles(tileMap2, 0, 0, 50, 50)
+        new Tiles(tileMap2, 0, 0, 50, 50);
     }
 
 }
