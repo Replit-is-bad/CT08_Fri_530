4b4a8d39a3320c669ef67b777c6fa522d2d352f6
Replit-is-bad <ylchia_hill@hotmail.com>
2026-08-07 09:51:21 +0000

    Aug 7, 2026, 9:51 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -94,7 +94,7 @@ function setup() {
     // Create map using tile map and sprite groups
     new Tiles(tileMap1, 0, 0, TILE_SIZE, TILE_SIZE);    // (map, x, y, width, height)
 
-    startSprite = new Sprite(width / 2 , height / 2.)
+    startSprite = new Sprite(width / 2 , height / 2, 190 , 90);
 }
 function resetGame() {
     box.rotation = 0;
