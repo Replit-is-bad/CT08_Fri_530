ce506986e2c0aa55bf210b2ddf26976d436d52cc
Replit-is-bad <ylchia_hill@hotmail.com>
2026-08-07 09:44:36 +0000

    Aug 7, 2026, 9:44 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -35,7 +35,7 @@ function preload() {
     bg = loadImage("assets/geobg.png");
     spike = loadImage("assets/spike.png");
 
-    
+    startGameImg
 
     tileMap1 = loadStrings("stages/tiles3.txt");
 }
