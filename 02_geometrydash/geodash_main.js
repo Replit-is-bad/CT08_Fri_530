e4b9901793322f87d80d26e40614ae9c52d37d9c
Replit-is-bad <ylchia_hill@hotmail.com>
2026-08-14 10:30:46 +0000

    Aug 14, 2026, 10:30 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -46,7 +46,7 @@ function preload() {
     tileMap1 = loadStrings("stages/tiles3.txt");
     tileMap2 = loadStrings("stages/tiles2.txt");
 
-    backgroundTrack = createAudio("audio/")
+    backgroundTrack = createAudio("audio/02_geometrydash/assets/stereo-madness.mp3")
 }
 
 function setup() {
