a3b0bbed889ad53414ed7039e38cd82203628e97
Replit-is-bad <ylchia_hill@hotmail.com>
2026-08-21 09:41:46 +0000

    Aug 21, 2026, 9:41 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -58,7 +58,7 @@ function preload() {
 
 function setup() {
     new Canvas(700, 600);   // (width, height)
-    world.gravity.y = 32;
+    world.gravity.y = 3;
 
     jumpChance = MAX_JUMP;
     // Player sprite
