606c4e4f9bac6f44f31e0c7e71b843b506aaed3d
Replit-is-bad <ylchia_hill@hotmail.com>
2026-08-28 10:27:37 +0000

    Aug 28, 2026, 10:27 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -61,9 +61,12 @@ function draw() {
 
         for (let i = 0; i < hands.length; i++){
             let hand = hands[i];
+            
+            for (let j = 0; j < hand.keypoints.length; j++){
 
-            let keypoint = hand.keypoints[6];
-            circle(keypoint.x,keypoint.y,10);
+                let keypoint = hand.keypoints[j];
+                circle(keypoint.x,keypoint.y,10);
+            }
         }
     }
 
