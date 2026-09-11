576e34d21c7a089f0749edd5d405f6a0c7d14982
Replit-is-bad <ylchia_hill@hotmail.com>
2026-09-11 10:03:13 +0000

    Sep 11, 2026, 10:03 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -70,7 +70,7 @@ function setup() {
 
     leftWall = new Sprite();
     leftWall.x = 0;
-    leftWall.y = 640;
+    leftWall.y = 320;
     leftWall.width = 10;
     leftWall.height = 640;
     leftWall.collider = "static"
