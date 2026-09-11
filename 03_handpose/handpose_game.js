8697daf6c20700e7fb590f0336e4287093cb7b68
Replit-is-bad <ylchia_hill@hotmail.com>
2026-09-11 10:16:36 +0000

    Sep 11, 2026, 10:16 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -64,7 +64,7 @@ function setup() {
     balloon.color = "rgb(255,0,0)";
     balloon.x =width / 2;
     balloon.y  =height/10;
-    balloon.bounciness = 1;
+    balloon.bounciness = 100;
     balloon.mass = 1;
     balloon.drag =0.5;
 
