1ee5d7f2db71cf2a20097a40205be88d1c6441ca
Replit-is-bad <ylchia_hill@hotmail.com>
2026-09-18 10:42:18 +0000

    Sep 18, 2026, 10:42 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -165,7 +165,7 @@ function draw() {
         }
 
         if (gameOver === true) {
-            gameOverSound.play()
+            gameOverSound.play();
             textSize(40);
             textAlign(CENTER , CENTER);
             fill("rgb(255, 8, 8)");
