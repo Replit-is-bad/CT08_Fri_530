67f3db69d9b24e37d96194b40e9cc3306216da3b
Replit-is-bad <ylchia_hill@hotmail.com>
2026-09-18 10:41:13 +0000

    Sep 18, 2026, 10:41 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -36,7 +36,7 @@ function preload() {
     handPose = ml5.handPose(options);
 
     bounceSound =createAudio("assets/LowBoing.mps");
-    gameOverSound = createAudio("assets")
+    gameOverSound = createAudio("assets/DunDunn.mp3");
 }
 
 function setup() {
