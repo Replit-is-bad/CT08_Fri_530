2bbc1cf8b9398219fec7a3e4e6b522ec5960d376
Replit-is-bad <ylchia_hill@hotmail.com>
2026-09-18 10:30:29 +0000

    Sep 18, 2026, 10:30 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -17,7 +17,7 @@ let gameStart = false;
 let gameOver = false;
 
 let score = 0;
-let cooldown =
+let cooldown = 500;
 //=========================================
 // Code
 //=========================================
