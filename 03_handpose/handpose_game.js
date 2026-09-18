f6546076b077b54fdf82be6153cf804c7e20d413
Replit-is-bad <ylchia_hill@hotmail.com>
2026-09-18 09:44:45 +0000

    Sep 18, 2026, 9:44 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -117,7 +117,8 @@ function draw() {
 
         textSize(26);
         fill("rgb(2, 2, 145)");
-         text('PRESS SPACE TO START', width/2, height*0.6);
+        text('PRESS SPACE TO START', width/2, height*0.6);
+        key
     }
 
     // Check if model detects hand
