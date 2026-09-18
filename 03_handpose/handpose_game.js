564ff459ce42bb032218c26f329acbffbee87041
Replit-is-bad <ylchia_hill@hotmail.com>
2026-09-18 09:59:16 +0000

    Sep 18, 2026, 9:59 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -151,6 +151,7 @@ function draw() {
             fill("rgb(145, 2, 2)");
             text('PRESS SPACE TO RESTART', width/2, height*0.6);
             keyPressed();
+            fingerTip.visible = false;
         }
     }
     
